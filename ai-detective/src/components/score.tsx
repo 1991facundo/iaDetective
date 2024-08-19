e6e2c6c08fffff1c82f94c664abfdf679b00739c
Facundo Fernandez <99399998+1991facundo@users.noreply.github.com>
2024-08-19 17:04:03 +0200

    working on the score component

---
+++ ai-detective/src/components/score.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../store/store';
+import { increment, reset } from '../store/slices/scoreSlice';
+
+//definimos el componente Score
+const Score: React.FC = () => {
+    // useSelector nos permite acceder al estado global de Redux
+    const score = useSelector((state: RootState) => state.score.value);
+    // useDispatch nos permite despachar acciones para cambiar el estado
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <h1>Current Score: {score}</h1>
+            <button onClick={() => dispatch(increment())}>Increase Score</button>
+            <button onClick={() => dispatch(reset())}>Reset Score</button>
+        </div>
+    );
+};
+
+export default Score;
